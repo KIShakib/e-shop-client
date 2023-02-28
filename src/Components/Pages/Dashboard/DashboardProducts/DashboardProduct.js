628624e7cc6038982b4e8047f4298f949143fcff
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-02-28 23:54:05 +0600

    ProductDetails And Dashboard Complete

--- src/Components/Pages/Dashboard/DashboardProducts/DashboardProduct.js
+++ src/Components/Pages/Dashboard/DashboardProducts/DashboardProduct.js
@@ -4,7 +4,7 @@ import { FaTrash } from 'react-icons/fa';
 const DashboardProduct = ({ product, deleteProduct }) => {
     return (
         <div className="w-[400px] h-[84px] flex items-center justify-between lg:px-7 px-3 border rounded border-[#6A64F1]">
-            <img className="rounded-t-lg p-2 w-16" src={product.img[0]} alt="product" />
+            <img className="rounded-t-lg p-2 w-16" src={product.img} alt="product" />
             <h2 className="font-semibold text-neutral">{product?.name}</h2>
             <button
                 onClick={() => deleteProduct(product._id)}
